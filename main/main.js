8234c04f2d17fab4834df3f137a7c3332c70edca
muzrea <muzrea@hotmail.com>
2017-06-24 11:01:15 +0800

    fitst commit

--- main/main.js
+++ main/main.js
@@ -1,10 +1,11 @@
 module.exports = function main(i) {
 	var m ;
 	var j=parseInt(i);
-	var n = 0;
-	for(var a=0,a<3,a++){
+	var n=0;
+	for(var a=0,a<i.length,a++){
 		m=parseInt(j%10);
 		n=n+m;
+		j=j-m;
 		j=parseInt(j/10);
 	}
 	return n;
