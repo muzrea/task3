a4eb238bdf3e44fe5eff9e1956b8f6028fb18c50
muzrea <muzrea@hotmail.com>
2017-06-24 10:41:33 +0800

    task3

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ module.exports = function main(i) {
 	var m ;
 	var j=parseInt(i);
 	var n = 0;
-	for(var a=0,a<i.length,a++){
+	for(var a=0,a<3,a++){
 		m=parseInt(j%10);
 		n=n+m;
 		j=parseInt(j/10);
