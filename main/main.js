c40e92ae94b8860ad7d7e755218957d32607068d
muzrea <muzrea@hotmail.com>
2017-06-24 11:23:02 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -1,13 +1,8 @@
 module.exports = function main(i) {
-	var m ;
-	var j=parseInt(i);
-	var n=0;
+	var result;
 	for(var a=0,a<i.length,a++){
-		m=j%10;
-		n=n+m;
-		j=j-m;
-		j=j/10;
+		result += parseInt(i[a]);
 	}
-	return n;
+	return result;
   // Write your cade here
 };
